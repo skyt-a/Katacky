dddd0960061df081776402902a717c89951ee3f3
RinGoku <yuyu80105@gmail.com>
2023-05-03 19:12:26 +0900

    fix

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -1,7 +1,7 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-const serviceAccount = require("/firebase-adminsdk.json");
+const serviceAccount = JSON.parse(process.env.NEXT_PUBLIC_ADMIN_SECRET!);
 export const firebaseAdmin =
   getApps()[0] ??
   initializeApp({
