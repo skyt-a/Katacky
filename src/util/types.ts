70470847880f3fb49d9a548001cb64c79be68e77
RinGoku <yuyu80105@gmail.com>
2023-05-28 23:05:31 +0900

    fix bug

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,5 +1,5 @@
+type NullToUndefined<T> = T extends null ? undefined : T;
+
 export type UnionNullToUndefined<T> = {
-  [K in keyof T]: T[K] extends unknown | null
-    ? Exclude<T[K], null> | undefined
-    : T[K];
+  [K in keyof T]: NullToUndefined<T[K]>;
 };
