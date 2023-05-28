de1c01c50b43bf0d92f926f88463b74557500c1f
Yuta Sakou <36734151+skyt-a@users.noreply.github.com>
2023-05-28 18:48:06 +0900

    更新ボタンの内部を修正

--- src/components/layout/RefreshButton.tsx
+++ src/components/layout/RefreshButton.tsx
@@ -1,13 +1,11 @@
 "use client";
-import { useRouter } from "next/navigation";
 import { RefreshIcon } from "~/components/icons/refresh";
 
 export const RefreshButton = () => {
-  const router = useRouter();
   return (
     <button
       onClick={() => {
-        router.refresh();
+        window.location.reload();
       }}
       className="p-3 h-12 w-12"
     >
